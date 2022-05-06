a3c6d3575ae4f2245a86a1e07b26b40554a0c07a
Katsuya Ueki <k.ueki@rb4.so-net.ne.jp>
2022-05-07 01:21:19 +0900

    fix to remove unnecessary

--- docker/db/sql/001_create_tables.sql
+++ docker/db/sql/001_create_tables.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS `chocopi`;
+CREATE DATABASE IF NOT EXISTS `tradings`;
 
-CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_1s (
+CREATE TABLE IF NOT EXISTS tradings.FX_BTC_JPY_1s (
                                                      `datetime` TIMESTAMP NOT NULL,
                                                      `open` DOUBLE NOT NULL,
                                                      `close` DOUBLE NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_1s (
     INDEX datetime_index(`datetime`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_1m0s (
+CREATE TABLE IF NOT EXISTS tradings.FX_BTC_JPY_1m0s (
                                                        `datetime` TIMESTAMP NOT NULL,
                                                        `open` DOUBLE NOT NULL,
                                                        `close` DOUBLE NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_1m0s (
     INDEX datetime_index(`datetime`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_3m0s (
+CREATE TABLE IF NOT EXISTS tradings.FX_BTC_JPY_3m0s (
                                                        `datetime` TIMESTAMP NOT NULL,
                                                        `open` DOUBLE NOT NULL,
                                                        `close` DOUBLE NOT NULL,
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_3m0s (
     INDEX datetime_index(`datetime`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_5m0s (
+CREATE TABLE IF NOT EXISTS tradings.FX_BTC_JPY_5m0s (
                                                        `datetime` TIMESTAMP NOT NULL,
                                                        `open` DOUBLE NOT NULL,
                                                        `close` DOUBLE NOT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_5m0s (
     INDEX datetime_index(`datetime`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_15m0s (
+CREATE TABLE IF NOT EXISTS tradings.FX_BTC_JPY_15m0s (
                                                         `datetime` TIMESTAMP NOT NULL,
                                                         `open` DOUBLE NOT NULL,
                                                         `close` DOUBLE NOT NULL,
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_15m0s (
     INDEX datetime_index(`datetime`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_30m0s (
+CREATE TABLE IF NOT EXISTS tradings.FX_BTC_JPY_30m0s (
                                                         `datetime` TIMESTAMP NOT NULL,
                                                         `open` DOUBLE NOT NULL,
                                                         `close` DOUBLE NOT NULL,
@@ -66,7 +66,7 @@ CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_30m0s (
     INDEX datetime_index(`datetime`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_1h0m0s (
+CREATE TABLE IF NOT EXISTS tradings.FX_BTC_JPY_1h0m0s (
                                                          `datetime` TIMESTAMP NOT NULL,
                                                          `open` DOUBLE NOT NULL,
                                                          `close` DOUBLE NOT NULL,
@@ -77,7 +77,7 @@ CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_1h0m0s (
     INDEX datetime_index(`datetime`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_2h0m0s (
+CREATE TABLE IF NOT EXISTS tradings.FX_BTC_JPY_2h0m0s (
                                                          `datetime` TIMESTAMP NOT NULL,
                                                          `open` DOUBLE NOT NULL,
                                                          `close` DOUBLE NOT NULL,
@@ -88,7 +88,7 @@ CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_2h0m0s (
     INDEX datetime_index(`datetime`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_4h0m0s (
+CREATE TABLE IF NOT EXISTS tradings.FX_BTC_JPY_4h0m0s (
                                                          `datetime` TIMESTAMP NOT NULL,
                                                          `open` DOUBLE NOT NULL,
                                                          `close` DOUBLE NOT NULL,
@@ -99,7 +99,7 @@ CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_4h0m0s (
     INDEX datetime_index(`datetime`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_12h0m0s (
+CREATE TABLE IF NOT EXISTS tradings.FX_BTC_JPY_12h0m0s (
                                                           `datetime` TIMESTAMP NOT NULL,
                                                           `open` DOUBLE NOT NULL,
                                                           `close` DOUBLE NOT NULL,
@@ -110,7 +110,7 @@ CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_12h0m0s (
     INDEX datetime_index(`datetime`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_24h0m0s (
+CREATE TABLE IF NOT EXISTS tradings.FX_BTC_JPY_24h0m0s (
                                                           `datetime` TIMESTAMP NOT NULL,
                                                           `open` DOUBLE NOT NULL,
                                                           `close` DOUBLE NOT NULL,
@@ -121,7 +121,7 @@ CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_24h0m0s (
     INDEX datetime_index(`datetime`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_72h0m0s (
+CREATE TABLE IF NOT EXISTS tradings.FX_BTC_JPY_72h0m0s (
                                                           `datetime` TIMESTAMP NOT NULL,
                                                           `open` DOUBLE NOT NULL,
                                                           `close` DOUBLE NOT NULL,
@@ -132,39 +132,3 @@ CREATE TABLE IF NOT EXISTS chocopi.FX_BTC_JPY_72h0m0s (
     INDEX datetime_index(`datetime`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS chocopi.positions (
-    `id` varchar(64) NOT NULL,
-    `is_active` tinyint(1) NOT NULL DEFAULT 1,
-    `ordered_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `executed_at` timestamp NULL DEFAULT NULL,
-    `canceled_at` timestamp NULL DEFAULT NULL,
-    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`),
-    KEY `id_index` (`id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS chocopi.orders (
-    `id` varchar(255) NOT NULL,
-    `side` tinyint(1) NOT NULL,
-    `price` double DEFAULT NULL,
-    `size` double DEFAULT NULL,
-    `order_type` tinyint(1) NOT NULL,
-    PRIMARY KEY(`id`),
-    KEY `id_index` (`id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS chocopi.executions (
-    `id` varchar(255) NOT NULL,
-    `side` tinyint(1) NOT NULL,
-    `price` double DEFAULT NULL,
-    `size` double DEFAULT NULL,
-    PRIMARY KEY(`id`),
-    KEY `id_index` (`id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS chocopi.position_size_balance (
-    `id` varchar(255) NOT NULL,
-    `size` double DEFAULT NULL,
-    PRIMARY KEY(`id`),
-    KEY `id_index` (`id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
